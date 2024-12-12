53ed8c7e37520fe34304a41bfd7fac74fa7e8b41
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-12-12 11:42:09 +0200

    add db

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import { initMongoDB } from './db/initMongoDB.js';
 import { startServer } from './server.js';
 
-startServer();
+const boostrap = async () => {
+  await initMongoDB();
+  startServer();
+};
+
+boostrap();
